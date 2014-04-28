a5e3badb762d3e47ba8d7a6dac9a72979a455fcb
Nicolas Faure <nikolas.faure@gmail.com>
2014-04-28 19:52:12 -0300

    Se generó la clase Asteroid

---
+++ src/main/java/org/uqbar/asteroids/components/Asteroid.java
@@ -0,0 +1,18 @@
+package org.uqbar.asteroids.components;
+
+
+import com.uqbar.vainilla.DeltaState;
+import com.uqbar.vainilla.GameComponent;
+import com.uqbar.vainilla.GameScene;
+
+public class Asteroid extends GameComponent<GameScene>{
+
+	
+	
+	
+	
+	@Override
+	public void update(DeltaState deltaState){
+		
+	}
+}
