10bb58773df62a479658a79e86ccf37b215418b3
Nicolas Faure <nikolas.faure@gmail.com>
2014-05-05 00:39:05 -0300

    Se agrego la nave, y se le comenzo a colocar la rotacion

--- src/main/java/org/uqbar/asteroids/components/Ship.java
+++ src/main/java/org/uqbar/asteroids/components/Ship.java
@@ -5,21 +5,48 @@ import org.uqbar.asteroids.scene.AsteroidsScene;
 import com.uqbar.vainilla.DeltaState;
 import com.uqbar.vainilla.MovableComponent;
 import com.uqbar.vainilla.appearances.Sprite;
+import com.uqbar.vainilla.events.constants.Key;
+import com.uqbar.vainilla.utils.Vector2D;
 
 public class Ship extends MovableComponent<AsteroidsScene>{
 
-	private double radialSpeed = 10;
+	private double radialSpeed = 150;
 	
 	
 	public Ship(){
-		super(Sprite.fromImage("images/ship.png").scale(0.5, 0.5),10,10);
+		super(Sprite.fromImage("images/ship.png"),10,10);
+		this.setVector(this.buildVector());
+		this.getVector().setX(1);
+		this.getVector().setY(0);
+		this.setX(200);
+		this.setY(300);
+		this.setSpeed(25);
 	}
 	
 	
+	private Vector2D buildVector() {
+		return new Vector2D(1,0);
+	}
+
+
 	@Override
 	public void update(DeltaState deltaState){
 		double advance = this.getSpeed() * deltaState.getDelta();
-		this.move(advance * this.getVector().getX(), advance * this.getVector().getY());
+		double radialSpeed = this.getRadialSpeed() * deltaState.getDelta();
+		
+		if(deltaState.isKeyBeingHold(Key.RIGHT)){
+			Sprite sprite = Sprite.fromImage("images/ship.png");
+			System.out.println("Antes:" + this.getVector().angle());
+			this.getVector().rotate(radialSpeed/100);
+			System.out.println("Despues:" + this.getVector().angle());
+			this.setAppearance(sprite.rotate(this.getVector().angle()));
+		}else if(deltaState.isKeyBeingHold(Key.LEFT)){
+			Sprite sprite = Sprite.fromImage("images/ship.png");
+			this.getVector().rotate(-1 * radialSpeed/100);
+			this.setAppearance(sprite.rotate(this.getVector().angle()));
+		}
+		
+		//this.move(advance * this.getVector().getX(), advance * this.getVector().getY());
 
 	}
 
--- src/main/java/org/uqbar/asteroids/scene/AsteroidsScene.java
+++ src/main/java/org/uqbar/asteroids/scene/AsteroidsScene.java
@@ -1,16 +1,21 @@
 package org.uqbar.asteroids.scene;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.uqbar.asteroids.components.Asteroid;
 import org.uqbar.asteroids.components.Bullet;
+import org.uqbar.asteroids.components.Ship;
 
+import com.uqbar.vainilla.GameComponent;
 import com.uqbar.vainilla.GameScene;
+import com.uqbar.vainilla.appearances.Rectangle;
 
 public class AsteroidsScene extends GameScene {
 
 	private final List<Asteroid> asteroids = new ArrayList<Asteroid>();
+	private Ship ship = new Ship();
 
 	public List<Asteroid> getAsteroids() {
 		return this.asteroids;
@@ -21,18 +26,35 @@ public class AsteroidsScene extends GameScene {
 		this.addComponent(asteroid);
 	}
 	
+	private void initializeBackground() {
+		GameComponent<GameScene> background = new GameComponent<GameScene>(new Rectangle(Color.BLACK, this.getGame().getDisplayWidth(), this.getGame().getDisplayHeight()),0 ,0);
+		this.addComponent(background);
+	}
+	
 	@Override
 	public void initializeComponents() {
+		this.initializeBackground();
+		this.addComponent(this.getShip());
 		this.addComponent(new Bullet());
 		for (int i = 0; i < 4; i++) {
 			Asteroid asteroid = new Asteroid();
 			this.addAsteroid(asteroid);
 			asteroid.setRndPosition(this.getGame().getDisplayWidth(), this.getGame().getDisplayHeight());
 		}
+		
 	}
 
 	public void removeAsteroid(Asteroid asteroid) {
 		this.asteroids.remove(asteroid);
 		asteroid.destroy();
+	}
+
+	public Ship getShip() {
+		return ship;
+	}
+
+	public void setShip(Ship ship) {
+		this.ship = ship;
+		this.addComponent(ship);
 	}
 }
